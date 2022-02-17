0a8085a8c5fe7c404bc8a97dadc659a61c6114a7
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2022-02-17 15:14:47 -0500

    fish tank different images

---
+++ public/scripts/entities/gold_fish.js
@@ -0,0 +1,8 @@
+class GoldFish extends Fish {
+
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/gold-fish.png'; // Set the image
+  }
+
+}
